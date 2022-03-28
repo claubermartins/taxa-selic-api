13caa470bf69681f7d9d87c9624d4b3093bc3066
Clauber Martins Pinto <clauber.pinto@mapa.magov.intranet>
2022-03-28 15:11:47 -0300

    iniciando o projeto

---
+++ src/main/java/com/taxaselic/TaxaSelicApiApplication.java
@@ -0,0 +1,13 @@
+package com.taxaselic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxaSelicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxaSelicApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taxaselic/TaxaSelicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taxaselic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxaSelicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
