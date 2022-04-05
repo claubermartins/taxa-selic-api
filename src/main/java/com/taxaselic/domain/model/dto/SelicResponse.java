ab88bb20db5998f7a3e868ef984a06b19354c315
Clauber Martins Pinto <clauber.pinto@mapa.magov.intranet>
2022-04-05 10:04:59 -0300

    corrigindo o mapeamento do endpoint para o Swagger

--- src/main/java/com/taxaselic/domain/model/dto/SelicResponse.java
+++ src/main/java/com/taxaselic/domain/model/dto/SelicResponse.java
@@ -1,5 +1,9 @@
 package com.taxaselic.domain.model.dto;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
